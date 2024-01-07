783439580d9dbfc527a8d4ffadda9ef959f463a1
Yure Nobrega <yurenobrega221b@gmail.com>
2024-01-07 12:19:09 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
